2e6c3260d0416e6cbf9bcdf6d40722b0b57ebd05
pran005 <pranshri0506@gmail.com>
2021-07-19 01:33:40 +0530

    Upgrades to main.c as per new stack

--- Device/main.c
+++ Device/main.c
@@ -7,6 +7,9 @@
 
 void init_onBoardLed(void);
 volatile static uint32_t gl_usb_intr_stat, gl_usb_tx_stat, gl_usb_rx_stat ;
+volatile uint32_t ControlXfer_Buffer[64]; 
+static USB_DeviceStack_context_t usb_dev_context; 
+
 static usb_drv_context_t usb_drv_context = 
 {
     .base = (USB0_Type*)USB0_BASE,
@@ -15,47 +18,24 @@ static usb_drv_context_t usb_drv_context =
     .EpIntMsk = 0xFF
 }; 
 	
+static USB_DeviceStack_context_t usb_dev_context =
+{
+    .driverContext = &usb_drv_context,
+    .ControlXfer.HostRxBuffer = ControlXfer_Buffer,
+};
+
+
+
 int main(void)
 {
-    init_onBoardLed(); 
-	
-    /* Configure USB GPIOs */ 
-    SYSCTL->RCGCGPIO |= (1u<<3) ; 
-    
-    while(!(SYSCTL->PRGPIO & (1u<<3))) ; 
-
-    GPIOD->AFSEL &= ~ ( (1u<<4) | (1u<<5) ) ; 
-    GPIOD->DEN   &= ~ ( (1u<<4) | (1u<<5) ) ; 
-    GPIOD->AMSEL |=   ( (1u<<4) | (1u<<5) ) ; 
+    init_onBoardLed(); 		
 		
-    /* Reset USB SRCR2 */ 
-    SYSCTL->SRCR2 |= (1u<<16) ;
+    initialize_usb_driver(&usb_drv_context);
+   
+    USBSetDevStackContext(&usb_drv_context, (void*) &usb_dev_context);
 
-    for (volatile uint8_t i=0; i<16; i++) ; 
 
-    SYSCTL->SRCR2 &= ~(1u<<16) ; 
-
-    /* Enable USB in SYSCTL */ 
-    SYSCTL->RCGCUSB |= (1<<0) ;
-
-    /* Enable USB PLL  */
-    SYSCTL->RCC2 &= ~(1u<<14); 
-
-    /* Setup USB PHY */ 
-    USB0->GPCS |= 0x03; 
-    
-    /* Clear Interrupt Status Registers */ 
-    gl_usb_intr_stat= USB0->IS ;
-    gl_usb_tx_stat = USB0->TXIS  ;
-    gl_usb_rx_stat = USB0->RXIS  ; 
-		
-    /* Initialize the USB IP */ 
-    USB0->IE    |= (1u<<0) | (1u<<1) | (1u<<2) | (1u<<3) | (1u<<5) ;
-    USB0->TXIE  |= 0xFF ;
-    USB0->RXIE  |= 0xFE ;
-    USB0->TXFIFOSZ |= 0x4; 
-   		
-    /* Enable 'em D+/D- Terminations */ 
+    /* Enable pull-up on D+ */ 
     USBDevice_Connect(&usb_drv_context);
 		
     /* You should know this one */
@@ -72,18 +52,19 @@ void USB0_Handler(void)
     gl_usb_tx_stat = USB0->TXIS;
     gl_usb_rx_stat = USB0->RXIS;
 
+    /* Call single USB Handler function */
     /* Check if there's TX/RX Interrupt asserted on EP0 */ 
     if(gl_usb_tx_stat & 0x01) 
     {
 	    /* Enumeration Handler */ 
-        ControlEP_Handler() ; 
+        ControlEP_Handler(&usb_drv_context) ; 
     }		
 }
 
 void init_onBoardLed(void)
 {
-    /* Enable GPIOF Clocks */ 
-    SYSCTL -> RCGCGPIO |= (1<<5) ;
+	/* Enable GPIOF Clocks */ 
+  SYSCTL -> RCGCGPIO |= (1<<5) ;
 	
 	/* Wait for the clocks to stabilize */
 	while(! ((SYSCTL->PRGPIO) & (1<<5)) ) ;
@@ -93,6 +74,5 @@ void init_onBoardLed(void)
 
 	/* Set direction -> output */
 	GPIOF->DIR = (1<<1) | (1<<2) | (1<<3) ;
-
+	
 }
-
